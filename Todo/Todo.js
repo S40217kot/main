ef2b1974196010a758c0c38ad9253203157cc28d
S40217kot <sou.ok.0217@gmail.com>
2025-08-10 19:53:26 +0900

    Update Todo.js

--- Todo/Todo.js
+++ Todo/Todo.js
@@ -1,1 +1,24 @@
-document.cookie = "username=sampleuser; eexpires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+const cookieForm = document.getElementById('cookie-form');
+const cookieList = document.getElementById('cookie-list');
+
+const loadCookies = () => {
+    const cookies = document.cookie.split('; ');
+    cookieList.innerHTML = '';
+    cookies.forEach(cookie => {
+        const [key, value] = cookie.split('=');
+        const listItem = document.createElement('li');
+        listItem.textContent = `${key}: ${value}`;
+        cookieList.appendChild(listItem);
+    });
+};
+
+// 初期表示
+loadCookies();
+
+// フォーム送信イベント
+cookieForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.getElementById('cookie-input').value;
+    document.cookie = `data=${input}; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT`;
+    loadCookies();
+});
